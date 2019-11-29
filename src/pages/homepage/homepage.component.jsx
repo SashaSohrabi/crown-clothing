d09f1009e8d216894defc3cb70d5bfd5cc7fb5e2
sashasohrabi <shb.sohrabi@gmail.com>
2019-11-29 16:14:32 +0200

    updated menu-items to navigate witRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => (
+const HomePage = prop => (
   <div className="homepage">
     <Directory />
+    {console.log(prop)}
   </div>
 );
 
