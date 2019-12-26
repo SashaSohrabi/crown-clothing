6a5dfa7cef6f49e9cdca33f0bcac071c4b05b2f2
sashasohrabi <shb.sohrabi@gmail.com>
2019-12-26 18:21:26 +0200

    adding form input component

--- src/components/sing-in/sign-in.component.jsx
+++ src/components/sing-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
 
 import "./sign-in.styles.scss";
 
@@ -31,16 +32,22 @@ class SingIn extends React.Component {
         <span>Sing in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input name="email" type="email" value={this.state.email} required />
-          <label>Email</label>
-          <input
+          <FormInput
+            name="email"
+            type="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <FormInput
             name="password"
             type="password"
             value={this.state.password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="password"
             required
           />
-          <label>Password</label>
           <input type="submit" value="Submit Form" />
         </form>
       </div>
