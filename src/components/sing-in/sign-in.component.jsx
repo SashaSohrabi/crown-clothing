7ed50308bf40269f2ef9c246f87365e6557be7e6
sashasohrabi <shb.sohrabi@gmail.com>
2019-12-26 18:37:06 +0200

    adding CustomButton component

--- src/components/sing-in/sign-in.component.jsx
+++ src/components/sing-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import "./sign-in.styles.scss";
 
@@ -48,7 +49,7 @@ class SingIn extends React.Component {
             label="password"
             required
           />
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit">Sing In</CustomButton>
         </form>
       </div>
     );
