32c9ac9106c111b04fa04cd4564214003e73e09b
Sasha Sohrabi <sasha@nxte.nl>
2020-03-27 22:25:46 +0200

    added temporary contact page

---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import image from "../../assets/construction.png"
+
+import "./contact.styles.scss";
+
+const ContactPage = () => (
+  <div className="construction-image"> 
+    <img src={image} alt="image" className="bounceInDown"/>
+  </div>
+  
+)
+
+export default ContactPage;
