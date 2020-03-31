1f9163fa1dd407cb9fe0961a852e9d3a74766913
Sasha Sohrabi <sasha@nxte.nl>
2020-03-31 20:24:18 +0300

    Updates

---
+++ precache-manifest.ccff9d03c3b0c98b2c638891f642654c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288bab8ea561cd5d6c8788739d377e97",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "5b36254be7bf981d6287",
+    "url": "/crown-clothing/static/css/main.e38d19bb.chunk.css"
+  },
+  {
+    "revision": "cc500d83ae4f5dbcdaa5",
+    "url": "/crown-clothing/static/js/2.2ad344bc.chunk.js"
+  },
+  {
+    "revision": "5b36254be7bf981d6287",
+    "url": "/crown-clothing/static/js/main.619559ab.chunk.js"
+  },
+  {
+    "revision": "063d2a4e291feacc239c",
+    "url": "/crown-clothing/static/js/runtime-main.4b8d1358.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crown-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
